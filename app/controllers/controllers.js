a484201ee3b3d07cd0f9fa622b0f18ff54705bb4
grahamcrackers <grogers12487@gmail.com>
2014-02-28 08:55:39 -0600

    added some stuff

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -53,5 +53,31 @@ app.controller('AlbumController', function($scope){
 
 });
 
+app.controller('HeaderCtrl', function($scope){
+    $scope.isError = false;
+    $scope.isWarning = false;
 
+    $scope.showError = function() {
+        $scope.messageText = 'This is an error!';
+        $scope.isError = true;
+        $scope.isWarning = false;
+    };
 
+    $scope.showWarning = function() {
+        $scope.messageText = 'Just a warning. Please carry on.';
+        $scope.isError = false;
+        $scope.isWarning = true;
+    };
+});
+
+app.controller('RestaurantTableCtrl', function($scope){
+    $scope.directory = [
+        {name: 'The Handsome Heifer', cuisine: 'BBQ'},
+        {name: 'Green\'s Green Greens', cuisine: 'Salads'},
+        {name: 'House of Fine Fish', cuisine: 'Seafood'}
+    ];
+
+    $scope.selectRestaurant = function(row) {
+        $scope.selectedRow = row;
+    }
+});
